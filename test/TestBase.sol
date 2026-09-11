1703e7c60ff438712f617f9a54b7bd18d700990b
mushee <musheelabs@proton.me>
2026-09-11 10:29:22 +0100

    Add dependency-free Foundry test base

---
+++ test/TestBase.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface Vm {
+    function prank(address) external;
+    function startPrank(address) external;
+    function stopPrank() external;
+    function warp(uint256) external;
+    function deal(address who, uint256 newBalance) external;
+    function expectRevert(bytes calldata) external;
+}
+
+abstract contract TestBase {
+    Vm internal constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    function assertTrue(bool condition, string memory message) internal pure {
+        require(condition, message);
+    }
+
+    function assertEq(uint256 a, uint256 b, string memory message) internal pure {
+        require(a == b, message);
+    }
+
+    function assertEq(address a, address b, string memory message) internal pure {
+        require(a == b, message);
+    }
+
+    function assertGt(uint256 a, uint256 b, string memory message) internal pure {
+        require(a > b, message);
+    }
+
+    function assertLt(uint256 a, uint256 b, string memory message) internal pure {
+        require(a < b, message);
+    }
+}
